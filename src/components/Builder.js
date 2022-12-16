1bbc4ca6001c8484069f7012157a020ec16b0eed
Orlando Jose Umanzor Zelaya <orlandojose729@gmail.com>
2022-12-16 15:51:35 -0600

    Refactor to function components

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -1,26 +1,18 @@
-import React from "react";
 import Section from "./Section";
 import sectionTemplates from "../objects/sectionTemplates";
 import '../styles/Builder.css'
 
-class Builder extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-        }
-    }
-    
-
-    render(){
-        return(
-            <div className="builder-container">
-                <div className="builder-header">OnePageCV</div>
-                {sectionTemplates.map(section=>{
-                    return <Section key={section.section} updateSection={this.props.updateSection} addSection={this.props.addSection} removeSection={this.props.removeSection} section={section} savedInfo={this.props.savedInfo}/>
-                })}
-            </div>
-        )
-    }
+function Builder(props) {
+    return (
+        <div className="builder-container">
+            <div className="builder-header">OnePageCV</div>
+            {sectionTemplates.map(section => {
+                return (
+                    <Section key={section.section} section={section} {...props} />
+                )
+            })}
+        </div>
+    )
 }
 
 export default Builder
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,54 +1,61 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import SubSection from "./SubSection";
 
-class Section extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            section: this.props.section,
-            subSectionId: (()=>{
-                let id = 0;
-                this.props.savedInfo.forEach(el=>{
-                    if(el.sectionName === this.props.section.section && el.data.length){
-                        id = el.data[el.data.length - 1].id
+function Section(props) {
+    const [section] = useState(props.section)
+    const [subSectionId, setSubSectionId] = useState()
+
+    const updateId = () => {
+        setSubSectionId(prevState => prevState.subSectionId + 1)
+    }
+
+    useEffect(() => {
+        let id = 0;
+        props.savedInfo.forEach(el => {
+            if (el.sectionName === props.section.section && el.data.length) {
+                id = el.data[el.data.length - 1].id
+            }
+        })
+        setSubSectionId(id)
+    }, [])
+
+
+    return (
+        <div className={section.section + " section-container"}>
+            <header>{section.section}</header>
+            <div className="info-container">{section.info}</div>
+            <ul className="subSection-container">
+                {props.savedInfo.map(section => {
+                    if (section.sectionName === section.section) {
+                        //Render Subsection for each element in DB data of crr section
+                        return section.data.map((savedData, index) => {
+                            return (
+                                <SubSection
+                                    key={index}
+                                    removeSection={props.removeSection}
+                                    updateSection={props.updateSection}
+                                    savedData={savedData}
+                                    subSectionId={savedData.id}
+                                    sectionTemplate={section}
+                                />
+                            )
+                        })
                     }
-                })
-                return id
-            })(),
-        }
-    }
+                    return null
+                })}
+            </ul>
 
-
-    updateId() {
-        this.setState(prevState => {
-            return {subSectionId : prevState.subSectionId + 1}
-        } )
-    }
-
-    render() {
-        return (
-            <div className={this.state.section.section + " section-container"}>
-                <header>{this.state.section.section}</header>
-                <div className="info-container">{this.state.section.info}</div>
-                <ul className="subSection-container">
-                    {this.props.savedInfo.map(section => {
-                        if (section.sectionName === this.state.section.section) {
-                            //Render Subsection for each element in DB data of crr section
-                            return section.data.map((savedData, index) => {
-                                return <SubSection key={index} removeSection={this.props.removeSection} updateSection={this.props.updateSection} savedData={savedData} subSectionId={savedData.id} sectionTemplate={this.state.section} />
-                            })
-                        }
-                        return null
-                    })}
-                </ul>
-
-                {(this.state.section.section === 'Personal Details') || (this.state.section.section === 'Professional Summary') ? <div></div> : <div className="addInput" onClick={() => {
-                    this.props.addSection(this.state.section, this.state.subSectionId)
-                    this.updateId()
-                }}>Add {this.state.section.add} +</div>}
-            </div>
-        )
-    }
+            {(section.section === 'Personal Details') || (section.section === 'Professional Summary')
+                ? <div></div>
+                : <div
+                    className="addInput"
+                    onClick={() => {
+                        props.addSection(section, subSectionId)
+                        updateId()
+                    }}
+                  >Add {section.add} +</div>}
+        </div>
+    )
 }
 
 export default Section
